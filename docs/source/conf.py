fb8ca48ac27aecdaa95bfd2bc33660de155b4903
Jeroen Brauns <brauns.jeroen@pm.me>
2021-04-10 21:15:52 +0200

    [new] initial setup documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -31,6 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
+    "sphinx_copybutton",
+    "sphinx_last_updated_by_git",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +50,33 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
+
+# Hide show page source button
+html_show_sourcelink = False
+
+# Remove Sphinx footer
+html_show_sphinx = False
+
+# Make ASPX
+# html_file_suffix = ".aspx"
+# html_link_suffix = ".aspx"
+# htmlhelp_file_suffix = ".aspx"
+# htmlhelp_link_suffix = ".aspx"
+
+# Change date time format
+# %A = Weekday as locale’s full name.
+# %d = Day of the month as a zero-padded decimal number.
+# %B = Month as locale’s full name.
+# %Y = Year with century as a decimal number.
+# %H = Hour (24-hour clock) as a zero-padded decimal number.
+# %M = Minute as a zero-padded decimal number.
+today_fmt = '%d %B %Y at %H:%M'
+
+html_last_updated_fmt = '%d %B %Y at %H:%M'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,7 +1,5 @@
 .. Wachtwoord Reset Tool documentation master file, created by
-   sphinx-quickstart on Sat Apr 10 19:46:52 2021.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+   Jeroen Brauns on Sat Apr 10 19:46:52 2021.
 
 Welcome to Wachtwoord Reset Tool's documentation!
 =================================================
@@ -11,10 +9,3 @@ Welcome to Wachtwoord Reset Tool's documentation!
    :caption: Contents:
 
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
